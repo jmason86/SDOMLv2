6cddd117f792a711f734280bd36d390081ea8f14
James Mason <jmason86@gmail.com>
2022-12-05 13:04:18 -0500

    Now stores lines data to zarr as well

--- convert_to_zarr/eve_fits_to_zarr.py
+++ convert_to_zarr/eve_fits_to_zarr.py
@@ -14,47 +14,57 @@ delta_seconds_1958_to_unix = Time('1958-01-01').unix_tai  # TAI's formal epoch s
 
 def eve_fits_to_zarr():
     eve_path = '/Volumes/Roci Extension/sdo/eve/'
-    #lines_to_zarr() # "lines" here refers to the extracted emission lines data product
+    lines_to_zarr(lines_output_filename=eve_path + '/lines/eve_lines.zarr') # "lines" here refers to the extracted emission lines data product
     spectra_to_zarr(path_to_input_data=eve_path + 'spectra/', 
                     spectra_output_filename=eve_path + '/spectra/eve_spectra.zarr') # and this is the full spectra data product
 
 
 def lines_to_zarr(lines_output_filename='eve_lines.zarr'):
-    lines = read_lines_source_file()
-    lines_zarr = store_lines_in_zarr(lines, lines_output_filename)
+    lines_input = read_lines_source_file()
+    store_lines_in_zarr(lines_input, lines_output_filename)
 
 
 def read_lines_source_file(lines_source_file=os.path.expanduser('~') + '/Dropbox/Research/Data/SDO/EVE/eve_lines_2010121-2014146 MEGS-A Mission Bare Bones.sav'):
     return readsav(lines_source_file)
 
 
-def store_lines_in_zarr(lines, lines_output_filename='eve.zarr'):
+def store_lines_in_zarr(lines_input, lines_output_filename='eve.zarr'):
     store = zarr.DirectoryStore(lines_output_filename)
-    compressor = Blosc(cname='zstd', clevel=5, shuffle=Blosc.BITSHUFFLE)
     root = zarr.group(store=store,overwrite=True)
 
-    format_lines_for_zarr(lines, root)
+    lines = root.create_group('Lines')
+    compressor = Blosc(cname='zstd', clevel=5, shuffle=Blosc.BITSHUFFLE)
+    time = lines.create_dataset('TIME_ISO', shape=(np.shape(lines_input['iso'])), dtype='<U23', compressor=compressor)
+    wavelength = lines.create_dataset('WAVELENGTH', shape=(np.shape(lines_input['wavelength'])), dtype='>f4', compressor=compressor)
+    logt = lines.create_dataset('LOG_T', shape=(np.shape(lines_input['logt'])), dtype='>f4', compressor=compressor)
+    ion = lines.create_dataset('ION', shape=(np.shape(lines_input['name'])), dtype='<U23', compressor=compressor)
+    irradiance = lines.create_dataset('IRRADIANCE', shape=(np.shape(lines_input['irradiance'])), dtype='>f4', compressor=compressor)
+
+    time.attrs['UNITS'] = 'Universal Coordinated Time (UTC) in ISO8601 format'
+    wavelength.attrs['UNITS'] = 'nm'
+    logt.attrs['UNITS'] = 'log_10 of temperature in Kelvin'
+    ion.attrs['NOTE'] = 'This is the _dominate_ ion in the spectral range integrated to create this line. There may be other emission lines blended in.'
+    irradiance.attrs['UNITS'] = 'W m^-2'
+
+    time[:] = lines_input['iso']
+    wavelength[:] = lines_input['wavelength']
+    logt[:] = lines_input['logt']
+    ion[:] = lines_input['name']
+    irradiance[:, :] = lines_input['irradiance'][:, :]
 
 
-def format_lines_for_zarr(lines, root):
-    pass # TODO Populate based on Meng's code
-
-
-def spectra_to_zarr(path_to_input_data, spectra_output_filename):
+def spectra_to_zarr(path_to_input_data, spectra_output_filename='eve_spectra.zarr'):
     remove_old_zarr_file(spectra_output_filename)
     
     input_filenames = glob.glob(path_to_input_data + 'EVS_L2_*.fit*')
     for filename in input_filenames:
         meta, data = read_spectra_source_file(filename) 
-        store_spectra_in_zarr(meta, data, spectra_output_filename) # TODO: need to prevent override of the .zarr file in this loop
-
-    pass # TODO: Populate
+        store_spectra_in_zarr(meta, data, spectra_output_filename) 
 
 
 def remove_old_zarr_file(spectra_output_filename): 
     if os.path.exists(spectra_output_filename):
         shutil.rmtree(spectra_output_filename)
-
 
 
 def read_spectra_source_file(filename):
