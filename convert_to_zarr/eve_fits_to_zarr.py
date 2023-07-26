807a9c3d1b9e1ea33bc7ab6f3bdac4168f295503
James Mason <jmason86@gmail.com>
2023-07-26 14:04:56 -0400

    Store lines and spectra together and process in batches

--- convert_to_zarr/eve_fits_to_zarr.py
+++ convert_to_zarr/eve_fits_to_zarr.py
@@ -5,28 +5,63 @@ import numpy as np
 from scipy.io import readsav
 import zarr
 from numcodecs import Blosc, Delta
-from astropy.table import Table
+from astropy.table import Table, vstack
 from astropy.time import Time
 import pandas as pd
+from datetime import datetime
 
+eve_path = '/Volumes/Roci Extension 1/sdo/eve/'
+eve_output_filename = eve_path + 'eve.zarr'
 delta_seconds_1958_to_unix = Time('1958-01-01').unix_tai  # TAI's formal epoch starts at 1958-01-01 while astropy uses unix_tai, which is from 1970-01-01
 
 
-def eve_fits_to_zarr():
-    eve_path = '/Volumes/Roci Extension/sdo/eve/'
-    lines_to_zarr(path_to_input_data=eve_path + '/lines/', 
-                  lines_output_filename=eve_path + '/lines/eve_lines.zarr') # "lines" here refers to the extracted emission lines data product
-    spectra_to_zarr(path_to_input_data=eve_path + 'spectra/', 
-                    spectra_output_filename=eve_path + '/spectra/eve_spectra.zarr') # and this is the full spectra data product
+def eve_fits_to_zarr(remove_old_zarr_file=False):    
+    meta_lines, data_lines, evl_filenames_to_process = read_lines()
+    meta_spectra, data_spectra, evs_filenames_to_process = read_spectra()
+
+    if remove_old_zarr_file: 
+        remove_old_zarr_file(eve_output_filename)
+        setup_zarr_file(eve_output_filename, meta_lines, data_lines, meta_spectra, data_spectra)
+    store_lines_in_zarr(meta_lines, data_lines, eve_output_filename)
+    store_spectra_in_zarr(meta_spectra, data_spectra, eve_output_filename)
+
+    log_processed_files(evl_filenames_to_process)
+    log_processed_files(evs_filenames_to_process)
 
 
-def lines_to_zarr(path_to_input_data, lines_output_filename='eve_lines.zarr'):
-    remove_old_zarr_file(lines_output_filename)
+def remove_old_zarr_file(output_filename): 
+    if os.path.exists(output_filename):
+        shutil.rmtree(output_filename)
+    
 
-    input_filenames = glob.glob(path_to_input_data + 'EVL_L2_*.fit*')
-    for filename in input_filenames:
-        meta, data = read_lines_source_file(filename) 
-        store_lines_in_zarr(meta, data, lines_output_filename) 
+def read_lines(): 
+    all_evl_filenames = sorted(glob.glob(eve_path + 'lines/EVL_L2_*.fit*'))
+    start_filename = get_start_filename_from_log(evl_or_evs='evl')
+    start_index = all_evl_filenames.index(start_filename)
+    end_index = min(start_index + 10, len(all_evl_filenames)) # batch to 10 files at a time
+    filenames_to_process = all_evl_filenames[start_index:end_index]
+
+    meta_all = Table()
+    data_all = Table()
+    for filename in filenames_to_process:
+        meta, data = read_lines_source_file(filename)
+        meta_all = vstack([meta_all, meta])
+        data_all = vstack([data_all, data])
+    return meta_all, data_all, filenames_to_process
+
+
+def get_start_filename_from_log(evl_or_evs='evl'): 
+    df = pd.read_csv(eve_path + '/processing_log.txt', names=['timestamp', 'processing', 'filename'])
+    if evl_or_evs == 'evl':
+        df = df[df['filename'].str.contains('EVL_')]
+        input_filenames = sorted(glob.glob(eve_path + 'lines/EVL_L2_*.fit*'))  
+    else: 
+        df = df[df['filename'].str.contains('EVS_')]
+        input_filenames = sorted(glob.glob(eve_path + 'spectra/EVS_L2_*.fit*')) 
+    
+    last_filename = df['filename'].iloc[-1]
+    filtered_filenames = [filename for filename in input_filenames if os.path.basename(filename) > os.path.basename(last_filename)]
+    return filtered_filenames[0]
 
 
 def read_lines_source_file(filename):
@@ -46,55 +81,20 @@ def read_lines_source_file(filename):
     return meta, data
 
 
-def store_lines_in_zarr(meta, data, lines_output_filename):
-    store = zarr.DirectoryStore(lines_output_filename)
-    root = zarr.group(store=store,overwrite=False)
+def read_spectra():
+    all_evs_filenames = sorted(glob.glob(eve_path + 'spectra/EVS_L2_*.fit*'))
+    start_filename = get_start_filename_from_log(evl_or_evs='evs')
+    start_index = all_evs_filenames.index(start_filename)
+    end_index = min(start_index + 10, len(all_evs_filenames)) # batch to 10 files at a time
+    filenames_to_process = all_evs_filenames[start_index:end_index]
 
-    if 'Lines' not in root:
-        lines = root.create_group('Lines')
-        compressor = Blosc(cname='zstd', clevel=5, shuffle=Blosc.BITSHUFFLE)
-        time = lines.create_dataset('TIME_ISO', shape=(np.shape(data['TIME_ISO'])), dtype='<U23', compressor=compressor)
-        wavelength = lines.create_dataset('WAVELENGTH', shape=(np.shape(meta['WAVE_CENTER'])), dtype='>f4', compressor=compressor)
-        logt = lines.create_dataset('LOG_T', shape=(np.shape(meta['LOGT'])), dtype='>f4', compressor=compressor)
-        ion = lines.create_dataset('ION', shape=(np.shape(meta['NAME'])), dtype='<U23', compressor=compressor)
-        blends = lines.create_dataset('BLENDS', shape=(np.shape(meta['BLENDS'])), dtype='<U23', compressor=compressor)
-        irradiance = lines.create_dataset('IRRADIANCE', shape=(np.shape(data['IRRADIANCE'])), dtype='>f4', compressor=compressor)
-
-        time.attrs['UNITS'] = 'Universal Coordinated Time (UTC) in ISO8601 format'
-        wavelength.attrs['UNITS'] = 'nm'
-        logt.attrs['UNITS'] = 'log_10 of temperature in Kelvin'
-        ion.attrs['NOTE'] = 'This is the _dominate_ ion in the spectral range integrated to create this line. There may be other emission lines blended in (see "blends").'
-        blends.attrs['NOTE'] = 'These are the other emission lines that are blended with the dominate emission listed in "ion".'
-        irradiance.attrs['UNITS'] = 'W m^-2'
-    else: 
-        lines = root['Lines']
-        time = lines['TIME_ISO']
-        wavelength = lines['WAVELENGTH']
-        logt = lines['LOG_T']
-        ion = lines['ION']
-        blends = lines['BLENDS']
-        irradiance = lines['IRRADIANCE']
-
-    time.append(data['TIME_ISO'].value)
-    wavelength.append(meta['WAVE_CENTER'].value)
-    logt.append(meta['LOGT'].value)
-    ion.append(meta['NAME'].value)
-    blends.append(meta['BLENDS'].value)
-    irradiance.append(data['IRRADIANCE'].value)
-
-
-def spectra_to_zarr(path_to_input_data, spectra_output_filename='eve_spectra.zarr'):
-    remove_old_zarr_file(spectra_output_filename)
-    
-    input_filenames = glob.glob(path_to_input_data + 'EVS_L2_*.fit*')
-    for filename in input_filenames:
-        meta, data = read_spectra_source_file(filename) 
-        store_spectra_in_zarr(meta, data, spectra_output_filename) 
-
-
-def remove_old_zarr_file(output_filename): 
-    if os.path.exists(output_filename):
-        shutil.rmtree(output_filename)
+    meta_all = Table()
+    data_all = Table()
+    for filename in filenames_to_process:
+        meta, data = read_spectra_source_file(filename)
+        meta_all = vstack([meta_all, meta])
+        data_all = vstack([data_all, data])
+    return meta_all, data_all, filenames_to_process
 
 
 def read_spectra_source_file(filename):
@@ -111,6 +111,55 @@ def read_spectra_source_file(filename):
     meta = meta['TIME_ISO', 'IRRADIANCE']
 
     return meta, data  # TODO: Could have a test here to make sure that these are returned in the expected order (meta is a table with length 1, for example)
+
+
+def setup_zarr_file(eve_output_filename, meta_lines, data_lines, meta_spectra, data_spectra):
+    store = zarr.DirectoryStore(eve_output_filename)
+    root = zarr.group(store=store, overwrite=False)
+
+    lines = root.create_group('Lines')
+    compressor = Blosc(cname='zstd', clevel=5, shuffle=Blosc.BITSHUFFLE)
+    time = lines.create_dataset('TIME_ISO', shape=(np.shape(data_lines['TIME_ISO'])), dtype='<U23', compressor=compressor)
+    wavelength = lines.create_dataset('WAVELENGTH', shape=(np.shape(meta_lines['WAVE_CENTER'])), dtype='>f4', compressor=compressor)
+    logt = lines.create_dataset('LOG_T', shape=(np.shape(meta_lines['LOGT'])), dtype='>f4', compressor=compressor)
+    ion = lines.create_dataset('ION', shape=(np.shape(meta_lines['NAME'])), dtype='<U23', compressor=compressor)
+    blends = lines.create_dataset('BLENDS', shape=(np.shape(meta_lines['BLENDS'])), dtype='<U23', compressor=compressor)
+    irradiance = lines.create_dataset('IRRADIANCE', shape=(np.shape(data_lines['IRRADIANCE'])), dtype='>f4', compressor=compressor)
+
+    time.attrs['UNITS'] = 'Universal Coordinated Time (UTC) in ISO8601 format'
+    wavelength.attrs['UNITS'] = 'nm'
+    logt.attrs['UNITS'] = 'log_10 of temperature in Kelvin'
+    ion.attrs['NOTE'] = 'This is the _dominate_ ion in the spectral range integrated to create this line. There may be other emission lines blended in (see "blends").'
+    blends.attrs['NOTE'] = 'These are the other emission lines that are blended with the dominate emission listed in "ion".'
+    irradiance.attrs['UNITS'] = 'W m^-2'
+
+    spectra = root.create_group('Spectra')
+    compressor = Blosc(cname='zstd', clevel=5, shuffle=Blosc.BITSHUFFLE)
+    time = spectra.create_dataset('TIME_ISO', shape=(np.shape(data_spectra)[0]), dtype='<U23', compressor=compressor)
+    irradiance = spectra.create_dataset('IRRADIANCE', shape=(np.shape(data_spectra['IRRADIANCE'])), dtype='>f4', compressor=compressor)
+
+    time.attrs['UNITS'] = meta_spectra['TIME_ISO'].value[0]
+    irradiance.attrs['UNITS'] = str(meta_spectra['IRRADIANCE'].value[0])
+
+
+def store_lines_in_zarr(meta, data, lines_output_filename):
+    store = zarr.DirectoryStore(lines_output_filename)
+    root = zarr.group(store=store,overwrite=False)
+
+    lines = root['Lines']
+    time = lines['TIME_ISO']
+    wavelength = lines['WAVELENGTH']
+    logt = lines['LOG_T']
+    ion = lines['ION']
+    blends = lines['BLENDS']
+    irradiance = lines['IRRADIANCE']
+
+    time.append(data['TIME_ISO'].value)
+    wavelength.append(meta['WAVE_CENTER'].value)
+    logt.append(meta['LOGT'].value)
+    ion.append(meta['NAME'].value)
+    blends.append(meta['BLENDS'].value)
+    irradiance.append(data['IRRADIANCE'].value)
 
 
 def convert_time(data):
@@ -133,21 +182,19 @@ def store_spectra_in_zarr(meta, data, spectra_output_filename):
     store = zarr.DirectoryStore(spectra_output_filename)
     root = zarr.group(store=store, overwrite=False)
     
-    if 'Spectra' not in root: 
-        spectra = root.create_group('Spectra')
-        compressor = Blosc(cname='zstd', clevel=5, shuffle=Blosc.BITSHUFFLE)
-        time = spectra.create_dataset('TIME_ISO', shape=(np.shape(data)[0]), dtype='<U23', compressor=compressor)
-        irradiance = spectra.create_dataset('IRRADIANCE', shape=(np.shape(data['IRRADIANCE'])), dtype='>f4', compressor=compressor)
-
-        time.attrs['UNITS'] = meta['TIME_ISO'].value[0]
-        irradiance.attrs['UNITS'] = str(meta['IRRADIANCE'].value[0])
-    else: 
-        spectra = root['Spectra']
-        time = spectra['TIME_ISO']
-        irradiance = spectra['IRRADIANCE']
+    spectra = root['Spectra']
+    time = spectra['TIME_ISO']
+    irradiance = spectra['IRRADIANCE']
     
     time.append(data['TIME_ISO'].value)
     irradiance.append(data['IRRADIANCE'].value)
+
+
+def log_processed_files(filenames): 
+    file = open(eve_path + '/processing_log.txt', 'a')
+    for filename in filenames: 
+        file.write('\n' + datetime.now().strftime('%Y-%m-%dT%H:%M:%S') + ',Processed file,' + filename)
+    file.close()
 
 
 if __name__ == "__main__":
